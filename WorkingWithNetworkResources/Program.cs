e0d561e6358dc06b1d0182429670bd0e5e9b9525
ryan <ryancumming7@gmail.com>
2022-05-12 16:43:05 +0100

    Added WorkingWithNetworkResources

---
+++ WorkingWithNetworkResources/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using static System.Console;
+using System.Net;
+
+namespace WorkingWithNetworkResources
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
