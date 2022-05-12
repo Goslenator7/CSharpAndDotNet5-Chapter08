402764fdb2436ad2d953073acde850414a1a85af
ryan <ryancumming7@gmail.com>
2022-05-12 16:38:04 +0100

    Added content for WorkingWithRanges

--- WorkingWithRanges/Program.cs
+++ WorkingWithRanges/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using static System.Console;
+using System.Collections.Generic;
 
 namespace WorkingWithRanges
 {
@@ -6,7 +8,24 @@ namespace WorkingWithRanges
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string name = "Samantha Jones";
+
+            int lengthOfFirst = name.IndexOf(' ');
+            int lengthOfSecond = name.Length - lengthOfFirst - 1;
+
+            string firstName = name.Substring(startIndex: 0, length: lengthOfFirst);
+
+            string lastName = name.Substring(startIndex: name.Length - lengthOfSecond, length: lengthOfSecond);
+
+            WriteLine($"First name: {firstName}, last name: {lastName}");
+
+            ReadOnlySpan<char> nameAsSpan = name.AsSpan();
+
+            var firstNameSpan = nameAsSpan[0..lengthOfFirst];
+
+            var secondNameSpan = nameAsSpan[^lengthOfSecond..^0];
+
+            WriteLine($"First name: {firstNameSpan.ToString()}, last name: {secondNameSpan.ToString()}");
         }
     }
 }
